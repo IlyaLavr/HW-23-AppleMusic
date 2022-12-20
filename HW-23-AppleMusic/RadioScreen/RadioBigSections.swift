b28687740320ff5bf6bb8b18b5fd98c1e1741279
Ilya <iilavr@mail.ru>
2022-12-20 18:46:41 +0300

    add model

---
+++ HW-23-AppleMusic/RadioScreen/RadioBigSections.swift
@@ -0,0 +1,20 @@
+//
+//  RadioBigSections.swift
+//  HW-23-AppleMusic
+//
+//  Created by Илья on 20.12.2022.
+//
+
+import SwiftUI
+
+struct RadioBigSections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RadioBigSections_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioBigSections()
+    }
+}
