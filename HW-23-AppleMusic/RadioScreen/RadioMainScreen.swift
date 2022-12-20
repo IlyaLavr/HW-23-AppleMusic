ce00a39b2e59b9f4a04f3fa75a24489caca03423
Ilya <iilavr@mail.ru>
2022-12-20 18:49:09 +0300

    add radio main screen

---
+++ HW-23-AppleMusic/RadioScreen/RadioMainScreen.swift
@@ -0,0 +1,30 @@
+//
+//  RadioMainScreen.swift
+//  HW-23-AppleMusic
+//
+//  Created by Илья on 20.12.2022.
+//
+
+import SwiftUI
+
+struct RadioMainScreen: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                RadioBigSections()
+                Divider()
+                    .padding([.leading,.trailing])
+                HStack {
+                RadioSmallSections()
+            }
+            .navigationTitle("Radio")
+        }
+    }
+}
+}
+struct RadioMainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioMainScreen()
+    }
+}
+
