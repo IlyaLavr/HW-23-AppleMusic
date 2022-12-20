f91cd48833d411952c9fe332be0fd6494c3e2685
Ilya <iilavr@mail.ru>
2022-12-20 18:31:10 +0300

    Initial Commit

---
+++ HW-23-AppleMusic/HW_23_AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_23_AppleMusicApp.swift
+//  HW-23-AppleMusic
+//
+//  Created by Илья on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_23_AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
