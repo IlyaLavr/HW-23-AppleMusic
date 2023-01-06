b386d31e4d4b3ab1301ed6bea5e18ee07c0046cf
Ilya <iilavr@mail.ru>
2023-01-06 17:25:27 +0300

    add blur view on all screen

---
+++ HW-23-AppleMusic/Sources/MainScreen/MusicPlayer/BlurViewAllScreen.swift
@@ -0,0 +1,19 @@
+//
+//  BlurViewAllScreen.swift
+//  HW-23-AppleMusic
+//
+//  Created by Илья on 06.01.2023.
+//
+
+import SwiftUI
+
+struct BlurViewAllScreen: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
+    }
+}
